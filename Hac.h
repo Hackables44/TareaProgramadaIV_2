2f95bd1d079cce3ef75614d4cddc7a94c61e5c11
TheCcromer <33296759+TheCcromer@users.noreply.github.com>
2018-06-08 00:03:25 -0600

    update de la clase hac

--- Hac.h
+++ Hac.h
@@ -3,16 +3,17 @@
 #ifndef _HAC_H
 #define _HAC_H
 
-#include "Vector.h"
-
-class HAC{
+#include "Elemento.h"
+#include "Lista.h"
+class Hac : public Agrupador{
 	private:
-		double similitudCoseno(Vector * v1, Vector * v2); /** - Este método calcula la similitud que existe entre 2 vectores */
-		double calcularProductoPunto(Vector * v1, Vector * v2); /** - Calcula y devuelve el producto punto de 2 vectores */
-		double calcularNorma(Vector * v); /** - Calcula la norma del vector recibido como parámetro, recibe */
-		
+		void crearMatrizDistancias(Elemento*,Elemento*);
+		double** matriz;
+		int obtenerTamayoLista(Lista*);
 	public:
-		double distanciaDeVectores(Vector * v1, Vector * v2); /** - Calcula la distancia entre ambos vectores restandole a 1 la similitud coseno obtenido entre ambos vectores */
+		Hac();
+		~Hac();
+		Lista* agrupar(Lista*);
 };
 
 #endif
