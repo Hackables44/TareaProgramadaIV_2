2495d4f915d8aaff5ec89d5ea5ffa1775fb211a7
Josh01B <37640012+Josh01B@users.noreply.github.com>
2018-06-10 16:27:23 -0600

    Reconocimiento de comentarios
    Reconocimiento de comentarios

--- Palabra.h
+++ Palabra.h
@@ -11,37 +11,37 @@ class Palabra : public Elemento{
 		int longitud;
 		
 	public:
-		//Constructor de la clase Palabra
+		/** Constructor de la clase Palabra */
 		Palabra();
 		
-		//Constructor por parametro de la clase Palabra
+		/** Constructor por parametro de la clase Palabra */
 		Palabra(const char*);
 		
-		//Constructor por copia de la clase Palabra
+		/** Constructor por copia de la clase Palabra */
 		Palabra(const Palabra&);
 		
 		~Palabra();
 		
 		Palabra* clonar();
 		
-		//Sobrecarga del operador == para ver si la palabra ya existe o no, 0 es false y cualquier otro numero es true
+		/** Sobrecarga del operador == para ver si la palabra ya existe o no, 0 es false y cualquier otro numero es true */
 		int operator==(const Palabra&);
 		
-		//Sobrecarga del operador + que en este caso lo usaremos para concatenar Palabras
+		/** Sobrecarga del operador + que en este caso lo usaremos para concatenar Palabras */
 		Palabra operator+(Palabra&);
 		
-		//Sobrecarga del operador = para asignar una palabra a otra
+		/** Sobrecarga del operador = para asignar una palabra a otra */
 		Palabra& operator=(const Palabra&);
 		
 		double distancia(Elemento*);
 		
-		//Retorna el atributo palabra de la clase Palabra
+		/** Retorna el atributo palabra de la clase Palabra */
 		 char* getPalabra();
 		
-		//Asigna un nuevo valor al atributo palabra de la clase Palabra
+		/** Asigna un nuevo valor al atributo palabra de la clase Palabra */
 		void setPalabra(const char*);
 		
-		//Metodo que me devuelve la longitud de la palabra en la clase Palabra
+		/** Metodo que me devuelve la longitud de la palabra en la clase Palabra */
 		int getLongitud();
 		
 		std::ostream& imprimir(std::ostream&);
