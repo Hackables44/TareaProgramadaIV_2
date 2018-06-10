5817d8b108cc2bf180f12e0426d769ebd501d353
Josh01B <37640012+Josh01B@users.noreply.github.com>
2018-06-10 14:41:31 -0600

    Doc Interna
    Para hacer las documentaciones tanto interna como externa.

--- Producto.h
+++ Producto.h
@@ -2,7 +2,9 @@
 #define _PRODUCTO_ABSTRACTO
 #include <cstring>
 class Producto {
-   public:	
+   public:
+   /** - Destructor virtual que puede ser utilizado por las clases derivadas de las clases Elemento, Agrupador y Visualizador que son
+   subclases de esta clase abstracta Producto */
       virtual ~Producto(){};	
 };
 #endif
